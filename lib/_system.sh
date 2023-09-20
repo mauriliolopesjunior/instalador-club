4c672718c5decdec6c13b94e1d0651adb3dd0ae3
plwdesign <128205620+plwdesign@users.noreply.github.com>
2023-09-20 10:54:34 -0300

    Update _system.sh

--- lib/_system.sh
+++ lib/_system.sh
@@ -281,7 +281,7 @@ system_node_install() {
   sleep 2
 
   sudo su - root <<EOF
-  curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+  curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
   apt-get install -y nodejs
   sleep 2
   npm install -g npm@latest
@@ -290,7 +290,7 @@ system_node_install() {
   wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
   sudo apt-get update -y && sudo apt-get -y install postgresql
   sleep 2
-  sudo timedatectl set-timezone America/Sao_Paulo
+  sudo timedatectl set-timezone America/Bahia
   
 EOF
 
